cb31a4612788dabc6aa10042f222e9b91bfb547e
ashustikov@kameleoon.com <ashustikov@kameleoon.com>
2023-08-04 06:20:30 -0400

    Initial commit

---
+++ src/main/java/ru/acorn/redisexp/RedisExpApplication.java
@@ -0,0 +1,13 @@
+package ru.acorn.redisexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/acorn/redisexp/RedisExpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.acorn.redisexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
